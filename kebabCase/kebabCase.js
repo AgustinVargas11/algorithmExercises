47c9fe2ca027e7f4288e0a71f630047a9a079cd0
Agustin Vargas <AgustinVargas11@users.noreply.github.com>
2017-10-25 10:21:35 -0500

    Update kebabCase.js

--- kebabCase/kebabCase.js
+++ kebabCase/kebabCase.js
@@ -6,15 +6,6 @@ export default function kebabCase(str) {
     } else if (!str.length) {
         throw new Error('Cannot accept empty string');
     }
-
-    str = str.replace(/' '/g, '');
-    const pattern = new RegExp(/([a-z][A-Z])|([a-z][_][a-z])/, 'g');
-
-    return str.replace(pattern, function (match) {
-        if (/[a-z][A-Z]/.test(match)) {
-            return `${match[0]}-${match[1]}`;
-        } else {
-            return `${match[0]}-${match[2]}`;
-        }
-    }).toLowerCase();
+    const pattern = new RegExp(/(\s|_|[a-z][A-Z])/, 'g')
+    return str.replace(pattern, match => /[a-z][A-Z]/g.test(match) ? `${match[0]}-${match[1]}` : '-').toLowerCase();
 }
